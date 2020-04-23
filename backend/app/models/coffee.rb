0884f845f98ee3e4f87c69d62d985364b8fc8608
93geraldtee <93geraldtee@gmail.com>
2020-04-23 14:20:10 -0400

    fix associations

--- backend/app/models/coffee.rb
+++ backend/app/models/coffee.rb
@@ -1,2 +1,4 @@
 class Coffee < ApplicationRecord
+    has_many :coffee_origins
+    has_many :origins, through: :coffee_origins
 end
--- backend/app/models/origin.rb
+++ backend/app/models/origin.rb
@@ -1,2 +1,4 @@
 class Origin < ApplicationRecord
+    has_many :coffee_origins
+    has_many :coffees, through: :coffee_origins
 end
