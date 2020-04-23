98824c1f4b606e6fb51f6dcc765160fef9ad25eb
93geraldtee <93geraldtee@gmail.com>
2020-04-23 14:31:13 -0400

    added seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -13,8 +13,12 @@
 #     {name: "Len's Coffee", bean: "Liberica", description: "Light/Medium Roast. Floral, fruity aroma and a slightly smoky taste."}
 # ])
 
-Origin.create([
-    {region: "Latin America", country: "Peru", description: "Almost all beans are arabica. Low to high altitudes produce coffee with mild to bright acidity levels. Typically floral aromas, rich sweetness and fruit."},
-    {region: "Latin America", country: "Various", description: "Mostly arabica beans are cultivated here. They range in flavor profiles from clean and less acidic to complex and fruity."},
-    {region: "South East Asia", country: "Philippines", description: "Various beans are produced here, but the rare Liberica beans are cultivated. Dense, smoky, chocolaty, fruity. Aroma of aniseed."}
-])
+# Origin.create([
+#     {region: "Latin America", country: "Peru", description: "Almost all beans are arabica. Low to high altitudes produce coffee with mild to bright acidity levels. Typically floral aromas, rich sweetness and fruit."},
+#     {region: "Latin America", country: "Various", description: "Mostly arabica beans are cultivated here. They range in flavor profiles from clean and less acidic to complex and fruity."},
+#     {region: "South East Asia", country: "Philippines", description: "Various beans are produced here, but the rare Liberica beans are cultivated. Dense, smoky, chocolaty, fruity. Aroma of aniseed."}
+# ])
+
+Origin.find_by_id(1).update(coffee_ids: [2])
+Origin.find_by_id(2).update(coffee_ids: [1, 2, 3])
+Origin.find_by_id(3).update(coffee_ids: [4])
